39bbb5b6895ffe69af9692a9b1773dd3fc5b352d
Shamsddine <goumdissshamsddine@gmail.com>
2023-10-25 16:15:57 +0200

    problemes affichage

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,25 @@
 
-fetch("../../Data/movies.json")
-.then(res => console.log(res.json()))
-.then(data => console.log(JSON.stringify(data)))
+fetch("Data/movies.json")
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    console.log(data.results);
 
+
+    for (const movie of data.results) {
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+        <img class="imgMovie" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${movie.title}">
+            <h2>${movie.title}</h2>
+            <p><i>${movie.release_date}</i></p>
+            <p>${movie.vote_average*10}%</p>
+        `;
+
+        infosContainer.appendChild(card);
+    }
+} )
 // import results from '../../Data/movies.json' assert { type: 'json' }
 // console.log(results)
