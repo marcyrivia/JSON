0ee78067275b74d45689c8b7fe99f92917b94a36
zirael <goumdissshamsddine@gmailcom>
2023-10-28 00:44:20 +0200

    fin header

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,14 +12,37 @@ fetch("Data/movies.json")
         card.classList.add('card');
 
         card.innerHTML = `
-        <img class="imgMovie" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${movie.title}">
-            <h2>${movie.title}</h2>
-            <p><i>${movie.release_date}</i></p>
-            <p>${movie.vote_average*10}%</p>
+        <div class="icon">
+        <i class="bi bi-plus-circle-fill icons"></i>
+    </div>
+    <img class="imgMovie" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${movie.title}">
+    <h2>${movie.title}</h2>
+    <p><i>${movie.release_date}</i></p>
+    <div class="rated">
+        <p class="rating">${movie.vote_average * 10}%</p>
+    </div>
+    
         `;
 
         infosContainer.appendChild(card);
+        
     }
 } )
+// Sélectionnez l'élément p contenant la note
+const ratingElement = document.querySelector('.rating');
+
+// Obtenez la valeur de la note en pourcentage
+const ratingValue = movie.vote_average * 10;
+
+// Appliquez la couleur en fonction de la plage de notes
+if (ratingValue < 35) {
+    ratingElement.style.color = 'red';
+} else if (ratingValue >= 35 && ratingValue < 60) {
+    ratingElement.style.color = 'orange';
+} else {
+    ratingElement.style.color = 'green';
+}
+
 // import results from '../../Data/movies.json' assert { type: 'json' }
 // console.log(results)
+
